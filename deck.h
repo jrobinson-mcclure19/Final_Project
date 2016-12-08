27b3371bc2f19475f2c8d1fd762851eec9f5c0ff
jrobinson-mcclure19 <jrobinson-mcclure19@my.whitworth.edu>
2016-12-07 16:04:00 -0800

    Add files via upload

---
+++ deck.h
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+class Deck
+{
+private:
+	int score;
+
+public:
+	deck(string);
+	void draw();
+	void countscore();
+
+};
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//deck()
+//shuffle()
+//dealing of the hand
+//ask for cards
+//grab cards
+//gofish
+//draw
+//score 
+// File output/ input
